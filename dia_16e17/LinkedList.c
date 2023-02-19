290ec2ef3c2f195afad56d33ecf7a4f45ed23106
João Paulo Preti <jppreti@gmail.com>
2023-02-19 19:10:50 -0400

    função enqueue

--- dia_16e17/LinkedList.c
+++ dia_16e17/LinkedList.c
@@ -3,92 +3,123 @@
 #include "LinkedList.h"
 #include "log.h"
 
-void init(LinkedList *list){
+void init(LinkedList *list)
+{
     log_info("Inicializando a lista");
-    log_trace("init ->");
+    log_trace("INIT ->");
     list->first = NULL;
     list->size = 0;
     log_debug("list->first: %p", list->first);
     log_debug("list->size: %d", list->size);
-    log_trace("init <-");
+    log_trace("INIT <-");
 }
 
-int enqueue(LinkedList *list, void *data){
-   log_info("Adicionando elemento a lista");
-   log_trace("init ->");
-   if(list == NULL){
-        log_error("Parametro invalido");
-        log_debug("list: %p", list);
-        return 1;
-   }
-   
+int enqueue(LinkedList *list, void *data)
+{
+    log_info("Adicionando elemento a lista");
+    log_trace("ENQUEUE ->");
+
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL) // caso não há espaço suficiente na RAM ele retorna valor negativo
+        return -1;
+    newNode->data = data;
+    newNode->next = NULL;
+    if(isEmpty(list)) //se a lista estiver vazia
+        list->first = newNode; //novo nó e primeiro
+    else {
+        Node *aux = list->first; //aux aponta para o primeiro
+        while(aux->next != NULL) //enquanto não for o último nó
+            aux = aux->next; //aux avança para o nó seguinte
+        aux->next = newNode; //último nó aponta para o novo nó
+    }
+    list->size++;
+    return 1;
+    log_trace("ENQUEUE <-");
 }
 
-void* dequeue(LinkedList *list){
+void *dequeue(LinkedList *list)
+{
     return NULL;
 }
 
-void* first(LinkedList *list){
+void *first(LinkedList *list)
+{
     return NULL;
 }
 
-void* last(LinkedList *list){
+void *last(LinkedList *list)
+{
     return NULL;
 }
 
-int push(LinkedList *list, void *data){
+int push(LinkedList *list, void *data)
+{
     return 0;
 }
 
-void* pop(LinkedList *list){
+void *pop(LinkedList *list)
+{
     return NULL;
 }
 
-void* top(LinkedList *list){
+void *top(LinkedList *list)
+{
     return NULL;
 }
 
-bool isEmpty(LinkedList *list){
+bool isEmpty(LinkedList *list)
+{
     log_info("Verificando se a lista esta vazia");
-    log_trace("isEmpty ->");
-    if(list == NULL){
+    log_trace("ISEMPTY ->");
+    if (list == NULL)
+    {
         log_error("Parâmetro inválido");
         log_debug("list: %p", list);
         return true;
     }
     log_debug("list->size: %d", list->size);
-    log_trace("isEmpty <-");
-    if(list->size == 0){
+    log_trace("ISEMPTY <-");
+    if (list->size == 0)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 
-int indexOf(LinkedList *list, void *data, compare equal){
+int indexOf(LinkedList *list, void *data, compare equal)
+{
     return 0;
 }
 
-void *getPos(LinkedList *list, int pos){
+void *getPos(LinkedList *list, int pos)
+{
     return NULL;
 }
 
-Node* getNodeByPos(LinkedList *list, int pos){
+Node *getNodeByPos(LinkedList *list, int pos)
+{
     return NULL;
 }
 
-int add(LinkedList *list, int pos, void *data){
+int add(LinkedList *list, int pos, void *data)
+{
     return 0;
 }
 
-int addAll(LinkedList *listDest, int pos, LinkedList *listSource){
+int addAll(LinkedList *listDest, int pos, LinkedList *listSource)
+{
     return 0;
 }
 
-void* removePos(LinkedList *list, int pos){
+void *removePos(LinkedList *list, int pos)
+{
     return NULL;
 }
 
-bool removeData(LinkedList *list, void *data, compare equal){
+bool removeData(LinkedList *list, void *data, compare equal)
+{
     return NULL;
 }
--- dia_16e17/LinkedListTeste.c
+++ dia_16e17/LinkedListTeste.c
@@ -3,15 +3,18 @@
 #include "LinkedList.h"
 #include "log.h"
 
-int main(){
-    //INICIALIZA A FUNÇÃO DE LOGS
+int main()
+{   
+    int aux = 123;
+    // INICIALIZA A FUNÇÃO DE LOGS
     log_set_level(LOG_TRACE);
 
-    //Criação da lista ligada
+    // Criação da lista ligada
     LinkedList lista;
-    
+
     init(&lista);
     isEmpty(&lista);
-    
+    enqueue(&lista, &aux);
+
     return EXIT_SUCCESS;
 }
