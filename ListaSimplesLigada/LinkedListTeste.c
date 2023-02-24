1f3e7420dcd35c1ee0e9e194250e5cd7bab1e5d4
Gabriel <gamotasantos2000@gmail.com>
2023-02-24 14:33:47 -0400

    alteracao nos nomes das pastas

--- ListaSimplesLigada/LinkedListTeste.c
+++ ListaSimplesLigada/LinkedListTeste.c
@@ -4,11 +4,13 @@
 #include "log.h"
 
 int main()
-{
-    log_set_level(LOG_TRACE); // INICIALIZA A FUNÇÃO DE LOGS
+{   
+    FILE *file = fopen("program.log","w");
+	
+	log_set_level(LOG_INFO);
+	log_add_fp(file, LOG_TRACE);
 
     LinkedList lista; // Criação da lista ligada
-
     init(&lista); // INICIA A LISTA COM PONTEIRO null E SIZE 0
 
     int *aux = (int *)malloc(sizeof(int));
