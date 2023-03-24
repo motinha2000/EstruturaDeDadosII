3cb080ffcd4f0f39591960189d29af2dba5c2ce6
Gabriel <gamotasantos2000@gmail.com>
2023-03-24 15:27:14 -0400

    Hash erro include listaDupla

---
+++ Hash/Hash.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Hash.h"
+
+void initHash(HashStruct *hashStruct) {
+    for (int i=0;i<MAX;i++) {
+    //chamando init de DoublyLinkedList para inicializar cada lista do vetor
+        init(&(hashStruct->hashes[i]));
+    }
+    hashStruct->size = 0;
+}
+
+bool isHashEmpty(HashStruct *hashStruct) {
+    return hashStruct->size==0;
+}
+
+int hash(char *key) {
+    int sum = 0;
+    // percorremos todos os caracteres da string passada
+    for (int i = 0; key[i]!=0;i++) {
+        //acumulamos os códigos ascii de cada letra com um peso
+        sum+=key[i]*(i+1);
+    }
+    return sum%MAX; //retorna o resto da divisão
+}
+
+int put(HashStruct *hashStruct, char *key, void *data, compare equal) {
+    if (!containsKey(hashStruct, key, equal)) {
+        //adiciona na fila que está na posição devolvida pela função hash
+        int res = enqueue(&hashStruct->hashes[hash(key)],data);
+        //incrementa a qtde de elementos baseado na quantidade inserida por enqueue
+        hashStruct->size+=res;
+        return res;
+    }
+    return 0;
+}
+
+bool containsKey(HashStruct *hashStruct, char *key, compare equal) {
+    //calcula a posição
+    int hashValue = hash(key);
+    //busca na fila a posição da chave
+    int pos = indexOf(&hashStruct->hashes[hashValue], key, equal);
+    return (pos!=-1)?true:false;
+}
+
+void* get(HashStruct *hashStruct, char *key, compare equal) {
+    // descobre em qual fila/lista está o dado
+    int hashValue = hash(key);
+    //first é nó sentinela, começamos do segundo nó
+    Node *aux = hashStruct->hashes[hashValue].first->next;
+    // procuramos o dado na lista
+    while(aux!=hashStruct->hashes[hashValue].first && !equal(aux->data, key))
+        aux=aux->next;
+    return aux->data;
+}
+
+void* removeKey(HashStruct *hashStruct, char *key, compare equal) {
+    int hashValue = hash(key);
+    int pos = indexOf(&hashStruct->hashes[hashValue], key, equal);
+    void* result = removePos(&hashStruct->hashes[hashValue], pos);
+    if (result!=NULL) hashStruct->size--;
+    return result;
+}
+
+void showHashStruct(HashStruct *hashStruct, printNode print) {
+    printf("There are %d elements in the Hash\n\n",hashStruct->size);
+    for (int i=0; i < MAX; i++) {
+        printf("Hash %d has %d elements: ",i,hashStruct->hashes[i].size);
+        show(&hashStruct->hashes[i],print);
+        printf("\n");
+    }
+}
