6a8bae52202688e42ce8b9f37e594881b7a0ec1e
Gabriel <gamotasantos2000@gmail.com>
2023-05-25 16:24:14 -0400

    qualquer

--- Arvore/tree.c
+++ Arvore/tree.c
@@ -1,12 +1,9 @@
 #include "tree.h"
-#include <stdlib.h>
 
-int add(TreeNode **root, void *element, TreeComparator f)
-{
-    if ((*root) == NULL)
-    {
+int add(TreeNode **root, void *element, TreeComparator f) {
+    if ((*root) == NULL) {
         // alcançou o local da folha, atualiza o pai
-        TreeNode *newnode = (TreeNode *)malloc(sizeof(TreeNode));
+        TreeNode *newnode = (TreeNode *) malloc(sizeof(TreeNode));
         if (newnode == NULL)
             return 0;
         newnode->element = element;
@@ -15,29 +12,22 @@ int add(TreeNode **root, void *element, TreeComparator f)
         return 1;
     }
     int compvalue = f(element, (*root)->element);
-    if (compvalue > 0)
-    {
+    if (compvalue > 0) {
         return add(&(*root)->right, element, f);
-    }
-    else if (compvalue < 0)
-    {
+    } else if (compvalue < 0) {
         return add(&(*root)->left, element, f);
-    }
-    else
-    {
+    } else {
         return -1;
     }
 }
 
-int find(TreeNode *root, void *key, TreeComparator f, void **element)
-{
+int find(TreeNode *root, void *key, TreeComparator f, void **element) {
     int compvalue;
     // Passamos a folha
     if (root == NULL)
         return 0;
     compvalue = f(key, root->element);
-    if (compvalue == 0)
-    {
+    if (compvalue == 0) {
         // encontrado
         *element = root->element;
         return 1;
@@ -48,44 +38,36 @@ int find(TreeNode *root, void *key, TreeComparator f, void **element)
     return find(root->left, key, f, element);
 }
 
-void in_order(TreeNode *root, printNode print)
-{
-    if (root != NULL)
-    {
+void in_order(TreeNode *root, printNode print) {
+    if (root!=NULL) {
         in_order(root->left, print);
         print(root->element);
         in_order(root->right, print);
     }
 }
 
-void pre_order(TreeNode *root, printNode print)
-{
-    if (root != NULL)
-    {
+void pre_order(TreeNode *root, printNode print) {
+    if (root!=NULL) {
         print(root->element);
         pre_order(root->left, print);
         pre_order(root->right, print);
     }
 }
 
-void post_order(TreeNode *root, printNode print)
-{
-    if (root != NULL)
-    {
+void post_order(TreeNode *root, printNode print) {
+    if (root!=NULL) {
         post_order(root->left, print);
         post_order(root->right, print);
         print(root->element);
     }
 }
 
-TreeNode *greaterRight(TreeNode **no)
-{
-    if ((*no)->right != NULL)
+TreeNode *greaterRight(TreeNode **no){
+    if((*no)->right != NULL)
         return greaterRight(&(*no)->right);
-    else
-    {
+    else{
         TreeNode *aux = *no;
-        if ((*no)->left != NULL) // se nao houver essa verificacao, esse nó vai perder todos os seus filhos da esquerda!
+        if((*no)->left != NULL) // se nao houver essa verificacao, esse nó vai perder todos os seus filhos da esquerda!
             *no = (*no)->left;
         else
             *no = NULL;
@@ -93,14 +75,12 @@ TreeNode *greaterRight(TreeNode **no)
     }
 }
 
-TreeNode *smallerLeft(TreeNode **no)
-{
-    if ((*no)->left != NULL)
+TreeNode *smallerLeft(TreeNode **no){
+    if((*no)->left != NULL)
         return smallerLeft(&(*no)->left);
-    else
-    {
+    else{
         TreeNode *aux = *no;
-        if ((*no)->right != NULL) // se nao houver essa verificacao, esse nó vai perder todos os seus filhos da direita!
+        if((*no)->right != NULL) // se nao houver essa verificacao, esse nó vai perder todos os seus filhos da direita!
             *no = (*no)->right;
         else
             *no = NULL;
@@ -108,57 +88,38 @@ TreeNode *smallerLeft(TreeNode **no)
     }
 }
 
-int removeTreeNode(TreeNode **root, void *key, TreeComparator f)
-{
-    if (*root == NULL)
-    { // caso a chave nao exista na arvore
+int removeTreeNode(TreeNode **root, void *key, TreeComparator f) {
+    if(*root == NULL){ // caso a chave nao exista na arvore
         return 0;
     }
     int compvalue = f(key, (*root)->element);
-    if (compvalue < 0)
+    if(compvalue < 0)
         removeTreeNode(&(*root)->left, key, f);
-    else
-    {
-        if (compvalue > 0)
-        {
+    else {
+        if(compvalue > 0) {
             removeTreeNode(&(*root)->right, key, f);
-        }
-        else
-        { // se nao eh igual
+        } else { // se nao eh igual
             TreeNode *aux = *root;
             // Se nao tem filhos
-            if (((*root)->left == NULL) && ((*root)->right == NULL))
-            {
+            if (((*root)->left == NULL) && ((*root)->right == NULL)){
                 free(aux);
                 (*root) = NULL;
-            }
-            else
-            { // so tem o filho da direita
-                if ((*root)->left == NULL)
-                {
+            } else { // so tem o filho da direita
+                if ((*root)->left == NULL){
                     (*root) = (*root)->right;
                     aux->right = NULL;
-                    free(aux);
-                    aux = NULL;
-                }
-                else
-                { // so tem filho da esquerda
-                    if ((*root)->right == NULL)
-                    {
+                    free(aux); aux = NULL;
+                } else { //so tem filho da esquerda
+                    if ((*root)->right == NULL){
                         (*root) = (*root)->left;
                         aux->left = NULL;
-                        free(aux);
-                        aux = NULL;
-                    }
-                    else
-                    { // Busco o maior filho à direita da subarvore esquerda.
+                        free(aux); aux = NULL;
+                    } else { //Busco o maior filho à direita da subarvore esquerda.
                         aux = greaterRight(&(*root)->left);
                         aux->left = (*root)->left;
                         aux->right = (*root)->right;
                         (*root)->left = (*root)->right = NULL;
-                        free((*root));
-                        *root = aux;
-                        aux = NULL;
+                        free((*root)); *root = aux; aux = NULL;
                     }
                 }
             }
@@ -167,28 +128,22 @@ int removeTreeNode(TreeNode **root, void *key, TreeComparator f)
     return 1;
 }
 
-int height(TreeNode *root)
-{
+int height (TreeNode *root) {
     if (root == NULL)
         return -1; // altura da árvore vazia
-    else
-    {
-        int hl = height(root->left);  // altura esquerda
-        int hr = height(root->right); // altura direita
-        if (hl < hr)
-            return hr + 1;
-        else
-            return hl + 1;
+    else {
+        int hl = height(root->left); //altura esquerda
+        int hr = height(root->right); //altura direita
+        if (hl < hr) return hr + 1;
+        else return hl + 1;
     }
 }
 
-// navegar em pos-ordem
-void destroy(TreeNode **root)
-{
-    if (*root == NULL)
-        return;
+//navegar em pos-ordem
+void destroy (TreeNode **root) {
+    if (*root==NULL) return;
     destroy(&(*root)->left);
     destroy(&(*root)->right);
     free(*root);
-    *root = NULL;
+    *root=NULL;
 }
