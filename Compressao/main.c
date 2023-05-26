8caadf12675f958932f65f159d852a74ffceef9b
Gabriel <gamotasantos2000@gmail.com>
2023-05-25 22:47:55 -0400

    COMENTARIOS ADICIONADOS

--- Compressao/main.c
+++ Compressao/main.c
@@ -14,23 +14,23 @@ int main(int argc, char *argv[])
 
     if (arquivo == NULL) printf("\nErro ao abrir o arquivo.\n");
 
-    while ((ch = fgetc(arquivo)) != EOF)
+    while ((ch = fgetc(arquivo)) != EOF)//PERCORRER O ARQUIVO E EXIBIR OS CARACTERES ENCONTRADOS
     {
-        if (ch != 32 && ch != 10)
+        if (ch != 32 && ch != 10)//IGNORAR ESPAÇOS E QUEBRAS DE LINHA
             printf("%c", ch);
     }
-    fseek(arquivo,0,SEEK_SET);
-    while ((ch = fgetc(arquivo)) != EOF && i<7)
+    fseek(arquivo,0,SEEK_SET);//MUDAR O PONTEIRO PRO COMEÇO DO ARQUIVO NOVAMENTE
+    while ((ch = fgetc(arquivo)) != EOF && i<7)//PERCORRER O ARQUIVO E RESPEITAR O LIMITE DO VETOR DE STRUCT
     {
-        if (ch != 32 && ch != 10){
-            h[i].representacao=ch;
-            i++;
+        if (ch != 32 && ch != 10){//IGNORAR ESPAÇOS E QUEBRAS DE LINHA
+            h[i].representacao=ch;//ATRIBUIR VALOR ENCONTRADO AO STRUCT
+            i++;//AVANÇAR POSIÇÃO NO STRUCT
         }
     }
 
     printf("\n");
 
-    for(int j=0;j<7;j++)
+    for(int j=0;j<7;j++)//EXIBIR OS CARACTERES PELO STRUCT
         printf("%c",h[j].representacao);
 
     fclose(arquivo);
