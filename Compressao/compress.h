27112a4231f094695a02f09dabb7bf97b9473a97
Gabriel <gamotasantos2000@gmail.com>
2023-05-25 21:18:49 -0400

    lendo caracter a caracter do arquivo com uma palavra

--- Compressao/compress.h
+++ Compressao/compress.h
@@ -3,7 +3,14 @@
 2° PASSO - VER FREQUÊNCIA DOS SIMBOLOS.
 3° PASSO - MONTAR A ARVORE DE BAIXO PRA CIMA
 MENOR FREQUÊNCIA -> MAIOR FREQUÊNCIA.
-4° PASSO -  EXIBIR SIMBOLOS E SUAS FREQUÊNCIAS E MOSTRAR 
+4° PASSO -  EXIBIR SIMBOLOS E SUAS FREQUÊNCIAS E MOSTRAR
 */
 #define MAX_TAMANHO_PALAVRAS 100
 #define CARACTERES 256
+
+typedef struct Simbolo
+{   
+    char representacao;
+    unsigned int frequencia;
+    struct Simbolo *endereço;
+}Simbolo;
--- Compressao/main.c
+++ Compressao/main.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[ ])
-{
+int main(int argc, char *argv[])
+{   
+    char c;
     FILE *arquivo = fopen(argv[1], "r");
-    printf("%s",argv[1]);
-    if (arquivo == NULL)
-    {
-        printf("\nErro ao abrir o arquivo.\n");
-        exit(1);
-    }
-    else printf("\nTUDO OK!");
+    printf("%s", argv[1]);
 
-    
+    if (arquivo == NULL) printf("\nErro ao abrir o arquivo.\n");
+    else printf("\nTudo ok!\n");
 
-    return 0;
+    while( (c=fgetc(arquivo)) !=EOF)
+        printf("%c", c);
+
+    fclose(arquivo);
 }
