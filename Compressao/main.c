1767d274dd35ef8cfe478c13cecbbeadef7912ce
Gabriel <gamotasantos2000@gmail.com>
2023-05-28 22:49:55 -0400

    Verificação da frequência OK

--- Compressao/main.c
+++ Compressao/main.c
@@ -1,6 +1,7 @@
 #include "compress.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #define MAX 11
 
 int main(int argc, char *argv[])
@@ -14,35 +15,63 @@ int main(int argc, char *argv[])
     if (arquivo == NULL)
         printf("\nErro ao abrir o arquivo.\n");
 
-    while ((ch = fgetc(arquivo)) != EOF) // PERCORRER O ARQUIVO E RESPEITAR O TAMANHO DO VETOR DE STRUCT
+    while ((ch = fgetc(arquivo)) != EOF) // CONTAR CARACTER
         cont++;
-    int *v = malloc(cont * sizeof(int));
-    printf("\nChar's: %d.", cont);
 
-    fseek(arquivo, 0, SEEK_SET); // MUDAR O PONTEIRO PRO COMEÇO DO ARQUIVO NOVAMENTE
+    int *v = malloc(cont * sizeof(int));
+    printf("\nChars: %d.", cont);
+
+    fseek(arquivo, 0, SEEK_SET);
     printf("\n");
-    while ((ch = fgetc(arquivo)) != EOF && i < cont) // PERCORRER O ARQUIVO E RESPEITAR O TAMANHO DO VETOR DE STRUCT
+    while ((ch = fgetc(arquivo)) != EOF && i < cont) // GUARDAR CARACTER COMO INT
     {
         v[i] = (int)ch;
-        printf("%c", (char)v[i]);
+        //printf("%c", (char)v[i]);
         i++;
     }
-    //SIMBOLO *h = malloc(cont*sizeof(SIMBOLO));
-    SIMBOLO *h = malloc(cont*sizeof(SIMBOLO));
+    SIMBOLO *h = malloc(cont * sizeof(SIMBOLO));
     for (int j = 0; j < cont; j++)
-        h[j].frequencia=0;
-    printf("\n");
-    fseek(arquivo, 0, SEEK_SET); // MUDAR O PONTEIRO PRO COMEÇO DO ARQUIVO NOVAMENTE
-    for (int j = 0; j < cont; j++)
-    {   
-        if(h[j].representacao==(char)v[j])
-            h[j].frequencia++;
-        else 
-            h[j].representacao = (char)v[j]; // ATRIBUIR VALOR ENCONTRADO AO STRUCT
-    }   
+        h[j].frequencia = 0;
 
-    for (int j = 0; j < cont; j++)
-        printf("%d", h[j].frequencia);
+    for (int j = 0; j < cont; j++) // ATRIBUIR AO STRUCT
+    {
+        h[j].representacao = (char)v[j];
+        for (int t = 0; t < cont; t++) // CALCULA FREQUENCIA
+        {
+            if (h[j].representacao == (char)v[t])
+                h[j].frequencia++;
+        }
+    }
 
+    for (int j = 0; j < cont; j++) // VERIFICAR REPETICOES E ANULAR O STRUCT
+    {
+        for (int t = 0; t < cont; t++)
+        {
+            if (j != t && h[j].representacao == h[t].representacao && h[j].frequencia != 0)
+                h[t].frequencia = 0;
+        }
+    }
+
+    for (int j = 0; j < cont; j++) // EXIBIÇÃO STRUCT NAO ANULADO
+    {
+        if (h[j].frequencia != 0)
+        {
+            if (h[j].representacao == (char)32)
+            {
+                printf("ESPAÇO(S) - ");
+                printf("%d\n", h[j].frequencia);
+            }
+            else if (h[j].representacao == (char)10)
+            {
+                printf("QUEBRA(S) DE LINHA - ");
+                printf("%d\n", h[j].frequencia);
+            }
+            else
+            {
+                printf("%c - ", h[j].representacao);
+                printf("%d\n", h[j].frequencia);
+            }
+        }
+    }
     fclose(arquivo);
 }
